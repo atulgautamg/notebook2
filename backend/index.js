cca0f86290c45ba97cd17a8ef6f99f8a6bca2b20
atulgautamg <125824896+atulgautamg@users.noreply.github.com>
2023-07-17 13:13:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const port = 5000
 app.use(express.json());
 app.use(cors());
 app.use(cors({
-  origin:{"https://notebook2-flame.vercel.app/"},
+  origin:{"https://notebook2-zegm.vercel.app/"},
   methods:{"POST","GET"},
   credidentials:true
   ));
